40f061d9b356423726842c50f9eb7c5ef2db51c0
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2023-12-07 22:54:28 +0100

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,45 @@
-import { concatenation } from "./concatenation.js";
-const button = document.querySelector("button");
-const input = document.querySelector("input");
-if (button && input) {
-  button.addEventListener("click", () => {
-    concatenation(input.value, "hello!");
-  });
+class Key {
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature() {
+        return this.signature;
+    }
 }
+class Person {
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
+    }
+}
+class House {
+    constructor(key) {
+        this.key = key;
+        this.tenants = [];
+    }
+    comeIn(person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log("You are comeIn");
+        }
+        else {
+            console.log("Your key is invalid");
+        }
+    }
+}
+class MyHouse extends House {
+    openDoor(key) {
+        if (this.key === key) {
+            this.door = true;
+        }
+    }
+}
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
+export {};
 //# sourceMappingURL=index.js.map
